567e411f4aad4f43b9c2a1c2348afca61a9b1c98
luizandre37 <lamaceno2005@gmail.com>
2022-06-07 22:05:39 -0300

    atualização

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '25111',
+                nome: 'Bruno Gabriel',
+                partido: 'ABC',
+                fotos: [
+                    { url: '38111.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '13222',
+                nome: 'Giseli Souza Paiva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '77222.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Luiz Andre',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito' },
+                    { url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'David Souza Paiva',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito' },
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
